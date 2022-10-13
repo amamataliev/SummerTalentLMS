bf42db239f734ad17b1d2840e1f5934543257d54
Aibek Mamataliev <aibek.mamataliev@gmail.com>
2022-10-13 20:26:23 +0600

    First Commit

---
+++ src/test/java/runner/RunnerClass.java
@@ -0,0 +1,20 @@
+package runner;
+
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "src/main/resources/features",
+        glue = "steps",
+        tags = "@smoke",
+        dryRun = true
+)
+
+public class RunnerClass {
+
+}
